9df1f02b9de2f0f04d38dd33c157df3f20759449
DomCastaneda <dcastaneda2828@gmail.com>
2022-10-06 19:51:59 -0700

    Update Terraformer File

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,13 +5,11 @@ public class BallActionHandler
    private Transform _playerCamera;
    private Rigidbody _ball;
    private float _baseBallThrust;
-   private float _yAxisSpawnPointAdjustment = 2.0f;
 
    public BallActionHandler(Transform playerCamera, Rigidbody ball, float baseBallThrust)
    {
       _playerCamera = playerCamera;
       _ball = ball;
-      _baseBallThrust = baseBallThrust;
    }
 
    // DEBUG: Use to debug what the object is overlapping with
--- main.tf
+++ main.tf
@@ -1,19 +1,12 @@
 terraform {
-  required_version = ">= 1.1.0"
   required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-  cloud {
-    organization = "CloudQuickLabs"
-    workspaces {
-      name = "AzureLabs"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.25.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
+provider "aws" {
+  region = "us-west-2"
 }
